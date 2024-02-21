422e3920e635060c3e68c0ed14ae016938a27363
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-02-21 10:55:45 -0300

    Finalizado

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-   <title>Calculadora</title>
+    <title>Formulário de Votação</title>
 </head>
 <body>
-    <h2>Calculadora Soma</h2>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];
-    ?>">
-        Núm 1: <input type="text" name="num1"><br><br>
-        <input type="submit" value="Somar">
+    <h2>Votação</h2>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <label>Digite sua idade:</label>
+        <input type="number" name="idade" required>
+        <br><br>
+        <input type="submit" value="Verificar Votação">
     </form>
 
     <?php
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['idade'])) {
+            $idade = $_POST['idade'];
 
-        if (!empty($_POST['num1']) && !empty($_POST['num2'])) {
-            $num1 = $_POST['num1'];
-            $num2 = $_POST['num2'];
-            
-            $soma = $num1 + $num2;
-
-            echo "<h3>A soma de $num1 e $num2 é: $soma</h3>";
+            if ($idade >= 16) {
+                echo "<h3>Você tem $idade anos e está apto para votar!</h3>";
+            } else {
+                echo "<h3>Você tem $idade anos. Desculpe, mas você não pode votar, pois é menor de 16 anos.</h3>";
+            }
         } else {
-            echo "<h3>Preencha todos os campos.</h3>";
+            echo "<h3>Por favor, digite sua idade antes de verificar a votação.</h3>";
         }
     }
     ?>
